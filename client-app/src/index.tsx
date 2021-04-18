673e4dd05d6f3e5e23a724556cfdb634cfc70716
oybekabdulazizov <s17712@pjwstk.edu.pl>
2021-04-18 17:15:41 +0200

    End of section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,15 +1,18 @@
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
-import { store, StoreContext } from './app/stores/store';
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
+import { store, StoreContext } from "./app/stores/store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
