238b81d7cfd07b967da5ab1861217fc6be1a8737
oybekabdulazizov <s17712@pjwstk.edu.pl>
2021-04-25 14:12:33 +0200

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,44 +4,57 @@ import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
 import HomePage from "../../features/home/HomePage";
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import { ToastContainer } from "react-toastify";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
-function App() {
-  const location = useLocation();
+export default function App() {
+    const location = useLocation();
 
-  return (
-    <>
-      <Route exact path="/" component={HomePage} />
-      <Route
-        path="/(.+)"
-        render={() => (
-          <>
-            <NavBar />
-            <Container style={{ marginTop: "7em" }}>
-              <Route
-                exact
-                path="/activities"
-                component={ActivityDashboard}
-              />
-              <Route
-                exact
-                path="/activities/:id"
-                component={ActivityDetails}
-              />
-              <Route
-                key={location.key}
-                exact
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-              />
-            </Container>
-          </>
-        )}
-      />
-    </>
-  );
+    return (
+        <>
+            <ToastContainer position='bottom-right' hideProgressBar />
+            <Route exact path="/" component={HomePage} />
+            <Route
+                path="/(.+)"
+                render={() => (
+                    <>
+                        <NavBar />
+                        <Container style={{ marginTop: "7em" }}>
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/activities"
+                                    component={ActivityDashboard}
+                                />
+                                <Route
+                                    exact
+                                    path="/activities/:id"
+                                    component={ActivityDetails}
+                                />
+                                <Route
+                                    key={location.key}
+                                    exact
+                                    path={["/createActivity", "/manage/:id"]}
+                                    component={ActivityForm}
+                                />
+                                <Route
+                                    path="/errors"
+                                    component={TestErrors} />
+                                <Route
+                                    path="/server-error"
+                                    component={ServerError} />
+                                <Route
+                                    component={NotFound} />
+                            </Switch>
+                        </Container>
+                    </>
+                )}
+            />
+        </>
+    );
 }
-
-export default observer(App);
